c07f0025a61d5ea0361d6db970853fdc40d0e33e
Weerawut Chaiyasomboon <weerawut@live.com>
2024-03-28 10:15:48 +0700

    Initial Commit

---
+++ SwiftfulSwitUIinPractice/SwiftfulSwitUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwitUIinPracticeApp.swift
+//  SwiftfulSwitUIinPractice
+//
+//  Created by Weerawut Chaiyasomboon on 28/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwitUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
